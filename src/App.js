be94c8a9d30f8a88c458272d607bca220b48adc4
code_spanol <85651178+spanol@users.noreply.github.com>
2023-03-08 23:27:25 -0300

    Timer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useRef } from "react";
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [timerOn, setTimerOn] = useState(false);
+  const intervalRef = useRef(null);
+  const Showtimer = () => {
+    return (
+      <div>
+        <h1>{formatTime(time)}</h1>
+      </div>
+    );
+  };
+
+  const formatTime = (time) => {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    return `${minutes < 10 ? "0" : ""}${minutes}:${
+      seconds < 10 ? "0" : ""
+    }${seconds}`;
+  };
+
+  const startTimer = () => {
+    setTimerOn(true);
+    intervalRef.current = setInterval(() => {
+      setTime((prevTime) => prevTime + 1);
+    }, 1000);
+  };
+
+  const pauseTimer = () => {
+    setTimerOn(false);
+    clearInterval(intervalRef.current);
+  };
+
+  const resetTimer = () => {
+    setTimerOn(false);
+    setTime(0);
+    clearInterval(intervalRef.current);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Showtimer />
+      <button onClick={startTimer}>Start</button>
+      <button onClick={pauseTimer}>Pause</button>
+      <button onClick={resetTimer}>Reset</button>
     </div>
   );
 }
