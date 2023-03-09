2fbc2c5650d0620a99eb680f20ed40b2781ceed6
code_spanol <85651178+spanol@users.noreply.github.com>
2023-03-08 22:55:45 -0300

    Dot tracker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
+// Crie um programa que ao clicar na tela, desenhe um ponto na tela. Ao clicar em um botão, limpe a tela. Ao clicar em outro botão, volte o último ponto desenhado.
+export default function App() {
+  const [dots, setDots] = useState([]);
 
-function App() {
+  const handleClick = (event) => {
+    const { clientX, clientY } = event;
+    setDots([...dots, { x: clientX, y: clientY }]);
+  };
+
+  const clearDots = (e) => {
+    e.stopPropagation();
+    setDots([]);
+  };
+
+  const back = (e) => {
+    e.stopPropagation();
+    const newDots = [...dots];
+    newDots.pop();
+    setDots(newDots);
+  };
+
+  const renderDots = () => {
+    return dots.map((dot, index) => {
+      return (
+        <div
+          className="dot"
+          key={index}
+          style={{
+            top: dot.y,
+            left: dot.x,
+          }}
+        />
+      );
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onClick={handleClick}>
+      <h1>Dot tracker</h1>
+      <h2>Start clicking in the screen and see the magic!</h2>
+      {dots.length > 0 &&
+      <div className="button-wrapper">
+       <button onClick={clearDots}>Clear</button>
+       <button onClick={back}>Back</button>
+      </div>
+      }
+      {dots && renderDots()}
     </div>
   );
 }
-
-export default App;
